680e5d42dc257198e3a20f53aff735e5fd0630a5
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-02-22 02:46:41 +0330

    used jinja(extends, block, url) & + base.html
    added base.html to templates and linked them to other pages using jinja, header links is not working fine, website url app_name is now know by 'website'

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,11 @@
+from unicodedata import name
 from django.urls import path
 from .views import *
 
+app_name = 'website'
+
 urlpatterns = [
-    path("", index_view),
-    path("about/", about_view),
-    path("contact/", contact_view)
+    path("", index_view, name="home"),
+    path("about/", about_view, name="about"),
+    path("contact/", contact_view, name="contact")
 ]
