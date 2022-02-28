a3343288b5177699f04acd7f18fd660edae196e5
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-02-28 18:10:36 +0330

    ModelAdmin has been set
    added some options to admin panel for post model

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from blog.models import Post
 
 # Register your models here.
-admin.site.register(Post)
+class postAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title', 'content', 'status', 'published_date', 'created_date')
+    list_filter = ('status',)
+    ordering = ['-created_date']
+    search_fields = ['title', 'content']
+    # exclude = ('content',)
+    # fields = ('content', 'title')
+admin.site.register(Post, postAdmin)
