a1c98a36f6760d901212b7426cd8070e9a3b3992
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-03-01 16:40:42 +0330

    added more detail to each post(author,image)
    + post's list_display of admin panel is now covered by id too
    + url pattern debugged (access denied for urls that has status=false)
    + author added to blog: post Models
    + from now author full name will be shown on single and home blog page
    + installed pillow page to use imagefield
    + created requirements.txt
    + media folder and media/blog folder created, default image also has been set into media/blog
    + blog pages now can have image for each specific post

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from blog.models import Post
 class postAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    list_display = ('title', 'content', 'status', 'published_date', 'created_date')
+    list_display = ('id', 'author', 'title', 'content', 'status', 'published_date', 'created_date')
     list_filter = ('status',)
     search_fields = ['title', 'content']
     # exclude = ('content',)
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
+    image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
     counted_views = models.IntegerField(default=0)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
