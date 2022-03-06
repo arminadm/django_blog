fe531c44067104b7da272caf7e051c8ab6416f1f
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-03-06 16:29:04 +0330

    created testform in WRONG WAY! (just to learn)
    + created testform.html
    + created testform path for websiteapp
    + created testform func in views

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from website.models import Contact
 # Create your views here.
 def index_view(request):
     return render(request, 'website/index.html')
@@ -9,3 +10,20 @@ def about_view(request):
 
 def contact_view(request):
     return render(request, "website/contact.html")
+
+def testFrom(request):
+    if request.method == 'GET':
+        return render(request, 'testForm.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        print(name, email, subject, message)
+        c = Contact()
+        c.name = name
+        c.email = email
+        c.subject = subject
+        c.message = message
+        c.save()
+        return HttpResponse('done')
