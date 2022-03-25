83f4800fe44e6d4d11bdb2024b103f8510e3c736
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-03-25 23:27:55 +0430

    clearing mess part2 adding captcha (finally)
    dont know how
    but captcha part that i wrote in the previous version didnt submitted, so here it is

--- website/forms.py
+++ website/forms.py
@@ -9,6 +9,7 @@ from captcha.fields import CaptchaField
 #     message = forms.CharField(widget=forms.Textarea)
 
 class contactForm(forms.ModelForm):
+    captcha = CaptchaField()
     class Meta:
         model = Contact
         fields = '__all__'
