40f033bcc4257d5ad10d136d100cd36c75bb77d0
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-03-28 19:33:14 +0430

    added signup feature to the website

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # Login
     path('login/', login_view, name='login'),
     # Logout
-    path('logout/', logout_view, name='logout')
+    path('logout/', logout_view, name='logout'),
     # Singup
+    path('signup/', signup_view, name='signup')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
+from multiprocessing import context
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -33,3 +34,34 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('/')
+
+def signup_view(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('/')
+            else:
+                form = UserCreationForm()
+                errors = [
+                    'required 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
+                    'Your password can’t be too similar to your other personal information.',
+                    'Your password must contain at least 8 characters.',
+                    'Your password can’t be a commonly used password.',
+                    'Your password can’t be entirely numeric.'
+                    
+                ]
+                context = {'form': form, 'errors': errors}
+                return render(request, 'accounts/signup.html', context)
+        if request.method == 'GET':
+            form = UserCreationForm()
+            context = {'form': form}
+            return render(request, 'accounts/signup.html', context)
+    else:
+        return redirect('/')
